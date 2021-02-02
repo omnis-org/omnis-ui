f561925bf6b6a943d5f131fc356d1e1a6150940e
Arthur Guyader <arthur@guyader.info>
2021-02-02 11:43:31 +0100

    Patch test location and perimeter service
    Signed-off-by: Arthur Guyader <arthur@guyader.info>
    Change-Id: I7eb60f962d028dea29ed5de869ba59df582572e6

--- src/app/@core/services/omnis/location.service.spec.ts
+++ src/app/@core/services/omnis/location.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LocationService } from './location.service';
 
-describe('LocationService', () => {
-  let service: LocationService;
-
+describe('NetworkService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LocationService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LocationService]
+    });
   });
 
-  it('should be created', () => {
+  it('should be created', inject([LocationService], (service: LocationService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
